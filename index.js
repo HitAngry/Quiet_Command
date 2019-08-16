6188478f302953b5e7cf9aef120d1e3653200ece
root <root@vps470641.ovh.net>
2019-08-16 16:47:34 +0200

    adding whitelist

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 let express = require('express'); 
 let bodyParser = require('body-parser');
 let config = require('./config');
+let axios = require('axios');
 
 let app = express();
-console.log(config);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
  
 app.post('/', (req, res) => {
-  console.log(req.body);
-  fetch(`//https://slack.com/api/users.info?token=${config.token}&user=${req.body.user_id}`)
-  .then(result => result.json)
+  axios(`https://slack.com/api/users.info?token=${config.token}&user=${req.body.user_id}`)
   .then(result => {
-    console.log(result);
+    if(config.whitelist.includes(result.data.user.profile.email)){
+      res.json({
+        "response_type": "in_channel",
+        "text":"<!channel> Be quiet please !"
+      });
+    } else {
+      res.json({
+       "response_type": "ephemeral",
+       "text":"You got no right for this"
+      });
+    }
   })
-  res.json({
-    "response_type": "in_channel",
-    "text":"<!channel> Be quiet please !"
-  });
 });
 
 app.listen(config.port, config.hostname);
